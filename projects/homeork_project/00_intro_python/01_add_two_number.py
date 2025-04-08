376ebfe9d0f78fc8672606e5a06e03330b6c0278
MalaikaEmaan2002 <160256756+MalaikaEmaan2002@users.noreply.github.com>
2025-04-08 23:00:22 +0500

    01_add_two_number.py

--- projects/homeork_project/00_intro_python/01_add_two_number.py
+++ projects/homeork_project/00_intro_python/01_add_two_number.py
@@ -1,1 +1,8 @@
+def add():
+  first_number = int(input("Enter first number:"))
+  second_number = int(input("Enter first number:"))
+  sum = int(first_number + second_number)
+  print(f"The total sum of {first_number} and {second_number} is {sum}")
 
+if __name__ == '__main__':
+ add()
